123cb7faa5941a57b1b47b586330fb299aa9c77b
roman vdovenko <roman.vdovenko@idapgroup.com>
2022-08-09 22:54:58 +0300

    Added migration and Seeder.

---
+++ backend/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Core\Number;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => Str::random(10),
+            'last_name' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'phone' => (new Number)->numberBetween(),
+            'password' => Hash::make('password'),
+            'created_at' => date("Y-m-d H:i:s", strtotime('now')),
+            'updated_at' => date("Y-m-d H:i:s", strtotime('now')),
+        ]);
+    }
+}
